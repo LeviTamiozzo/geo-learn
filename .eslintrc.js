5ab1f9c49d5f002f84c26b2570d17b0a57f7e14c
Levi Tamiozzo <levi.tzzo@gmail.com>
2019-10-30 09:21:42 -0300

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,15 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+  plugins: ["es-beautifier"],
+  extends: ["plugin:vue/essential", "plugin:es-beautifier/standard", "@vue/typescript"],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "comma-dangle": ["error", "never"],
+    "linebreak-style": "off"
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "@typescript-eslint/parser"
   }
-}
+};
