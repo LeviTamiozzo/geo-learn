74a00ac9955d8bdd4d793e323fb9b6a26287830f
Levi Tamiozzo <levi.tzzo@gmail.com>
2019-10-30 09:28:23 -0300

    vuetify added

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,15 @@ module.exports = {
   env: {
     node: true
   },
-  plugins: ["es-beautifier"],
-  extends: ["plugin:vue/essential", "plugin:es-beautifier/standard", "@vue/typescript"],
+  'extends': [
+    'plugin:vue/essential',
+    '@vue/standard'
+  ],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    "comma-dangle": ["error", "never"],
-    "linebreak-style": "off"
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
-    parser: "@typescript-eslint/parser"
+    parser: 'babel-eslint'
   }
-};
+}
