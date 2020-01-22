c491b75d310fad0ca60c0709a29977ef4e35823e
LeviTamiozzo <levi.tzzo@gmail.com>
2020-01-22 09:25:38 -0300

    some new staff

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <v-content>
+        <v-container>
+        
+        </v-container>
+        </v-content>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
